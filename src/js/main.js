53cd705b76d387fca0c7bf20fa8936f39bb81d20
alvastern <alva.stern02@gmail.com>
2026-02-03 13:06:16 +0100

    Lade till funktionen init för att visa datan

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,26 @@
 "use strict"
 
-// Funktion för att hämta data från API med try/catch och async/await
+// Funktioner för att hämta data från API med try/catch och async/await
 async function getData() {
     try {
-        const response = await fetch("https://webbutveckling.miun.se/files/ramschema.json");
+        const response = await fetch("ramschema.json");
         const data = await response.json();
 
         return data;
+
     } catch (error) {
         console.error('Det uppstod ett fel', error.message);
     }
 }
+
+getData();
+
+async function init() {
+    const data = await getData();
+    console.log('Hämtad data', data)
+    renderTable(courses);
+}
+
+init();
+
+
